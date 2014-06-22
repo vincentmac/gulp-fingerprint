ce812ba2672c0eb011cac0f41944aec2e5a023d9
Andrey Yamanov <tenphi@gmail.com>
2014-06-22 15:38:06 +0400

    Add `replace` mode & Update tests

--- index.js
+++ index.js
@@ -21,8 +21,9 @@ var plugin = function(manifest, options) {
   // var regex = new RegExp('url\\("(.*)"\\)|src="(.*)"|href="(.*)"|url\\(\'(.*)\'\\)|src=\'(.*)\'|href=\'(.*)\'', 'g');
   var regex = /(?:url\(["']?(.*?)['"]?\)|src=["'](.*?)['"]|src=([^\s\>]+)(?:\>|\s)|href=["'](.*?)['"]|href=([^\s\>]+)(?:\>|\s))/g;
   var prefix = '';
-  var base;
+  var base = '';
   var strip = '';
+  var mode = 'regex';
   var content = [];
 
   // Use custom RegExp
@@ -30,26 +31,63 @@ var plugin = function(manifest, options) {
 
   if (options.prefix) prefix = options.prefix;
 
-  if (options.base) base = new RegExp('^\/' + options.base + '|^' + options.base);
+  if (options.base) base = options.base.replace(/^\//, '');
 
-  if (options.strip) strip = options.strip;
+  if (options.strip) strip = options.strip.replace(/^\//, '');
 
-  function removeBase(str, base) {
-    return str && str.slice(0, base.length).replace(base, '') + str.slice(base.length);
+  if (options.mode === 'replace') {
+    mode = 'replace';
   }
 
-  function urlReplace(buf, enc, cb) {
+  if (strip) {
+    var stripRegex = new RegExp('^\/' + strip + '|^' + strip);
+  }
+
+  if (base) {
+    var baseRegex = new RegExp('^\/' + base + '|^' + base);
+  }
+
+  function regexMode(buf, enc, cb) {
     var line = buf.toString();
 
     line = line.replace(regex, function(str, i) {
       var url = Array.prototype.slice.call(arguments, 1).filter(function(a) {return a;})[0];
       if (options.verbose) gutil.log(PLUGIN_NAME, 'Found:', chalk.yellow(m.replace(/^\//, '')));
       var replaced = manifest[url] || manifest[url.replace(/^\//, '')] || manifest[url.split(/[#?]/)[0]];
-      if (!replaced && base) replaced = manifest[removeBase(url, base)];
-      if (replaced) str = str.replace(url, prefix + removeBase(replaced, strip.replace(/^\//, '')));
+      if (!replaced && base) replaced = manifest[url.replace(baseRegex, '')];
+      if (replaced) str = str.replace(url, prefix + replaced.replace(stripRegex, ''));
       if (options.verbose) gutil.log(PLUGIN_NAME, 'Replaced:', chalk.green(line));
       return str;
     });
+
+    content.push(line);
+    cb();
+  }
+
+  function replaceMode(buf, enc, cb) {
+    var line = buf.toString();
+
+    base = base.replace(/(^\/|\/$)/g, '');
+
+    for (var url in manifest) {
+      var dest = manifest[url], replaced, bases;
+      if (strip) {
+        replaced = prefix + dest.replace(stripRegex, '');
+      } else {
+        replaced = prefix + dest;
+      }
+      bases = ['/', ''];
+      if (base) {
+        bases.unshift('/' + base + '/', base + '/');
+      }
+      for (var i = 0; i < bases.length; i++) {
+        var newLine = line.split(bases[i] + url).join(replaced);
+        if (line !== newLine) {
+          line = newLine;
+          break;
+        }
+      }
+    }
 
     content.push(line);
     cb();
@@ -74,7 +112,7 @@ var plugin = function(manifest, options) {
     if (file.isBuffer()) {
       // console.log('is Buffer');
       file.pipe(split())
-      .pipe(through(urlReplace,  function(callback) {
+      .pipe(through(mode === 'regex' ? regexMode : replaceMode,  function(callback) {
         if (content.length) {
           file.contents = new Buffer(content.join('\n'));
           that.push(file);
